82c46e0d481c48fe4b1e98a86a2ede83d08108d2
Benjamin Kinga <somethingscripted@gmail.com>
2020-12-11 01:05:50 -0600

    add solution w/o recursion

--- AoC2020/10__Adapter_Array/sol.py
+++ AoC2020/10__Adapter_Array/sol.py
@@ -32,3 +32,22 @@ def dp(k):
 
 
 print(dp(0))
+
+# Without Recursion:
+
+
+def valid_set(data, start, dp_arr):
+    total = dp_arr[start + 3]
+    for i in range(start, start + 3):
+        if i < 0:
+            continue
+        if data[start + 3] - data[i] <= 3:
+            total += dp_arr[i]
+    return total
+
+
+dp_arr = [0 for _ in range(len(data))]
+dp_arr[0] = 1
+for i in range(len(data)):
+    dp_arr[i] = valid_set(data, i-3, dp_arr)
+print(dp_arr[-1])
